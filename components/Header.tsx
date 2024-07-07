2ee7c4484530a6ecf76dbe21527624ac87a30552
Randy, Huang <gg875437@gmail.com>
2024-07-07 15:57:42 +0900

    chore: Update header layout and add authentication integration

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,17 +1,31 @@
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 import Link from "next/link";
 import Avatar from "./Avatar";
 
 function Header() {
   return (
-    <header>
-      <Link href="/">
+    <header
+      className="bg-white shadow-sm text-gray-800 
+      flex justify-between p-5"
+    >
+      <Link href="/" className="flex items-center text-4xl font-thin">
         {/* Avatar */}
-        <Avatar seed="1" className="w-12 h-12 rounded-full" />
-        <div>
+        <Avatar seed="1" className="" />
+        <div className="space-y-1">
           <h1>Assistly</h1>
           <h2 className="text-sm">Chat AI Agent</h2>
-        </div>{" "}
+        </div>
       </Link>
+
+      <div className="flex items-center">
+        <SignedIn>
+          <UserButton showName />
+        </SignedIn>
+
+        <SignedOut>
+          <SignInButton />
+        </SignedOut>
+      </div>
     </header>
   );
 }

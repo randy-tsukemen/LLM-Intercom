54a45d4606fb45b2ce00704cbbf1546c977b63c0
Randy, Huang <gg875437@gmail.com>
2024-07-07 16:29:32 +0900

    chore: Add utility function for merging CSS classes

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,3 +1,4 @@
+import { BotMessageSquare } from "lucide-react";
 import Link from "next/link";
 
 function Sidebar() {
@@ -10,7 +11,11 @@ function Sidebar() {
             className="hover:opacity-50 flex flex-col text-center
           lg:text-left lg:flex-row items-center gap-2 p-5 rounded-md bg-[#2991EE]"
           >
-            Link
+            <BotMessageSquare className="w-6 h-6 lg:h-8 lg:w-8" />
+            <div className="hidden md:inline">
+              <p className="text-xl">Create</p>
+              <p className="text-sm font-extralight">New Chatbot</p>
+            </div>
           </Link>
         </li>
         <li className="flex-1">
